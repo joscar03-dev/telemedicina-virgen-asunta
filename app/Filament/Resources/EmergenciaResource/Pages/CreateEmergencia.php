250c7a14612fa7ab43b22b405bd7e46a258fea23
joscar03-dev <locomancocapac@gmail.com>
2024-12-04 07:54:03 -0500

    recurso emergencia

---
+++ app/Filament/Resources/EmergenciaResource/Pages/CreateEmergencia.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\EmergenciaResource\Pages;
+
+use App\Filament\Resources\EmergenciaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateEmergencia extends CreateRecord
+{
+    protected static string $resource = EmergenciaResource::class;
+}
